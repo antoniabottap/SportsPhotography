6b1c4ed161751898bbc4cdf96f9a14655f084e9e
antoniabottap <latotibotta@gmail.com>
2024-12-10 20:40:12 -0500

    delete

--- PhotographyPortfolio/script.js
+++
@@ -1,9 +0,0 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.carousel-images img');
-slides[currentSlide].classList.add('active');
-
-function changeSlide(direction) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + direction + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
